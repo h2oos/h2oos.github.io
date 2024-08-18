b49e8b7318bae24118ecc0fa169dabf0587ed7ac
h2oos <h2oos@proton.me>
2024-08-18 17:21:01 -0400

    ...

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ---
+// js
 const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
 const smallMenu = document.querySelector('.header__sm-menu')
 const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
